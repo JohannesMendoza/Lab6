e1f1a91fe5d0d40ba55ce0b5f6484916de7189da
Jomar <Jomar@134.154.60.189>
2019-03-01 16:13:20 -0800

    Committing in New Branch

--- src/lab6/Lab6.java
+++ src/lab6/Lab6.java
@@ -20,6 +20,7 @@ public class Lab6 {
         System.out.println("I wrote this statement in my original file");
         System.out.println("I copy/pasted this statement in my original file");
         System.out.println("I wrote this in the Master Branch");
+        System.out.println("I wrottee this in my new Branch");
         // TODO code application logic here
     }
     
